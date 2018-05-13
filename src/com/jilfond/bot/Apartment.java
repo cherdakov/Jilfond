0ffe370f7b3ffd740371a967ce90e5dd04b70a55
Cherdakov <eqx11@yandex.ru>
2018-05-13 21:39:09 +0700

    add Aparmet's class

---
+++ src/com/jilfond/bot/Apartment.java
@@ -0,0 +1,12 @@
+package com.jilfond.bot;
+
+public class Apartment {
+    private String street;
+    private Integer number;
+
+    public Apartment(String street, Integer number) {
+        this.street = street;
+        this.number = number;
+    }
+
+}
--- src/com/jilfond/bot/Saler.java
+++ src/com/jilfond/bot/Saler.java
@@ -1,4 +1,14 @@
 package com.jilfond.bot;
 
+import java.util.List;
+
 public class Saler extends User {
+
+    List<Apartment> apartmentList;
+    public void addAppartment(Apartment apartment){
+        apartmentList.add(apartment);
+    }
+    public List<Apartment> getApartmentList() {
+        return apartmentList;
+    }
 }
