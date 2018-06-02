954f9c9784491c01e73901ade97d6a120a1e0f22
Cherdakov <eqx11@yandex.ru>
2018-06-03 00:16:06 +0700

    now message delete together apartment

--- main/src/com/jilfond/bot/managers/CallbackManager.java
+++ main/src/com/jilfond/bot/managers/CallbackManager.java
@@ -1,5 +1,6 @@
 package com.jilfond.bot.managers;
 
+import com.jilfond.bot.Bot;
 import com.jilfond.bot.databases.Database;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
@@ -8,15 +9,16 @@ import java.sql.SQLException;
 
 public class CallbackManager {
     Database database;
-
+    Bot bot = Bot.getCurrentBot();
     public CallbackManager(Database database) {
         this.database = database;
     }
 
     public void pushUpdate(Update update) {
-        System.out.println(update.getCallbackQuery().toString());
         try {
             database.deleteApartmentById(Integer.valueOf(update.getCallbackQuery().getData()));
+            Message message = update.getCallbackQuery().getMessage();
+            bot.deleteMessage(message.getMessageId(),message.getChatId());
         } catch (SQLException e) {
             e.printStackTrace();
         }
