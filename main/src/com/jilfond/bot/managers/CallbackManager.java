3d1df6a56b4cb6adf89f7cba96de79050edb7bcb
Cherdakov <eqx11@yandex.ru>
2018-06-11 23:21:49 +0700

    Добавил кнопку "Получить контактные данные"

--- main/src/com/jilfond/bot/managers/CallbackManager.java
+++ main/src/com/jilfond/bot/managers/CallbackManager.java
@@ -2,6 +2,7 @@ package com.jilfond.bot.managers;
 
 import com.jilfond.bot.Bot;
 import com.jilfond.bot.databases.Database;
+import com.jilfond.bot.objects.BotUser;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 
@@ -10,6 +11,7 @@ import java.sql.SQLException;
 public class CallbackManager {
     Database database;
     Bot bot = Bot.getCurrentBot();
+
     public CallbackManager(Database database) {
         this.database = database;
     }
@@ -20,22 +22,36 @@ public class CallbackManager {
         Integer id = Integer.valueOf(callback.split(" ")[1]);
         Message message = update.getCallbackQuery().getMessage();
         try {
-        switch (command){
-            case "deleteWish":
-                bot.deleteMessage(message.getMessageId(),message.getChatId());
-                database.deleteWishById(id);
-                break;
-            case "deleteApartment":
-                database.deleteApartmentById(id);
-                bot.deleteMessage(message.getMessageId(),message.getChatId());
-                break;
+            switch (command) {
+                case "deleteWish":
+                    bot.deleteMessage(message.getMessageId(), message.getChatId());
+                    database.deleteWishById(id);
+                    break;
+                case "deleteApartment":
+                    database.deleteApartmentById(id);
+                    bot.deleteMessage(message.getMessageId(), message.getChatId());
+                    break;
+                case "getUser":
+                    BotUser user = database.getBotUserByTelegramId(id);
+                    String contactDetails = user.getContact();
+                    if (!message.hasPhoto()) {
+                        bot.updateText(
+                                message.getChatId(),
+                                message.getMessageId(),
+                                message.getText() + "\n" + contactDetails,
+                                null);
+                    } else {
+                        bot.updateCaption(
+                                message.getChatId(),
+                                message.getMessageId(),
+                                message.getCaption() + "\n" + contactDetails,
+                                null);
+                    }
 
-        }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
 
 
     }
